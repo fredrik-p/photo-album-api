f4666945a47f92783a3995318f22f2cfe2f40c7f
Fredrik P <fredrik.pekkarinen@gmail.com>
2021-04-23 01:37:12 +0200

    sql and postman collection

---
+++ heroku_dc2f0e980143976_2021-04-23.sql
@@ -0,0 +1,131 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Värd: eu-cdbr-west-01.cleardb.com (MySQL 5.6.50-log)
+# Databas: heroku_dc2f0e980143976
+# Genereringstid: 2021-04-22 23:35:30 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Tabelldump albums
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `albums`;
+
+CREATE TABLE `albums` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `albums` WRITE;
+/*!40000 ALTER TABLE `albums` DISABLE KEYS */;
+
+INSERT INTO `albums` (`id`, `title`, `user_id`)
+VALUES
+	(5,'Aussie',35),
+	(15,'Nature',45);
+
+/*!40000 ALTER TABLE `albums` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Tabelldump albums_photos
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `albums_photos`;
+
+CREATE TABLE `albums_photos` (
+  `album_id` int(11) NOT NULL,
+  `photo_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `albums_photos` WRITE;
+/*!40000 ALTER TABLE `albums_photos` DISABLE KEYS */;
+
+INSERT INTO `albums_photos` (`album_id`, `photo_id`)
+VALUES
+	(5,95),
+	(5,85),
+	(15,65),
+	(15,75);
+
+/*!40000 ALTER TABLE `albums_photos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Tabelldump photos
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `photos`;
+
+CREATE TABLE `photos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `comment` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `photos` WRITE;
+/*!40000 ALTER TABLE `photos` DISABLE KEYS */;
+
+INSERT INTO `photos` (`id`, `title`, `url`, `comment`, `user_id`)
+VALUES
+	(65,'Forrest','https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1951&q=80','Some nice trees',45),
+	(75,'Waterfall','https://images.unsplash.com/photo-1433086966358-54859d0ed716?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80','Amazing waterfall',45),
+	(85,'Sydney','https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80','My fav city',35),
+	(95,'Aussie highway','https://images.unsplash.com/photo-1494233892892-84542a694e72?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80','Curvy road',35);
+
+/*!40000 ALTER TABLE `photos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Tabelldump users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+
+INSERT INTO `users` (`id`, `email`, `password`, `first_name`, `last_name`)
+VALUES
+	(35,'fred@pekkarinen.se','$2b$10$0wyVpOtHq87i1Eomu3dM5OjB1.XIVzg3lOAKvBIcz98oYgaCHBfkq','Fred','Pekkarinen'),
+	(45,'evan@tax.se','$2b$10$K7OSlmn1FDUGh62tVCEgFeArWoWTssLeAWBEcMbcdKmWIzyg41M.2','Evan','Norling');
+
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
