bccb630cf612a0aacdca2bb19525d60e4a61b863
Fredrik P <fredrik.pekkarinen@gmail.com>
2021-04-22 20:27:04 +0200

    added cors

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const logger = require('morgan');
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
 
-const app = express();
+const app = express()
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(logger('dev'))
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.use(require('./routes'))
 
-module.exports = app;
+module.exports = app
